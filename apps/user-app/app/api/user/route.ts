e8bd4e77ed85edd27d85b4ba9282bb45828500a6
Ritik Jadhav <ritiksjadhav99@gmail.com>
2024-07-31 16:54:55 +0530

    import prisma issue fix

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -1,15 +1,13 @@
-import { PrismaClient } from '@tranzact/db';
+import prisma from '@tranzact/db';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 
-// const client = new PrismaClient()
-
 export const GET = async () => {
-    // await client.user.create({
-    //     data: {
-    //         email: 'ritik@gmail.com',
-    //         name: 'Ritik'
-    //     }
-    // })
+    await prisma.user.create({
+        data: {
+            email: 'ritik@gmail.com',
+            name: 'Ritik'
+        }
+    })
     return NextResponse.json({ message: 'Hi there!'})
 }
