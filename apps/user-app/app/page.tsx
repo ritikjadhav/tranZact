56ccd90e353859a405f463972c7fa478ecd8350d
Ritik Jadhav <ritiksjadhav99@gmail.com>
2024-08-04 23:18:23 +0530

    added Appbar ui component

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,18 @@
 "use client"
+
+import { Button } from '@repo/ui/button';
+import { Appbar } from '@repo/ui/Appbar';
 import { useBalance } from '@tranzact/store/useBalance';
 
 export default function Home() {
   const balance = useBalance()
+  const user = {
+    name: 'ritik'
+  }
   return (
-    <div>Your balance is: {balance}</div>
+    <div>
+      <Appbar user={user} onSignin={() => {}} onSignout={() => {}}/>
+      <div>Your balance is: {balance}</div>
+    </div>
   );
 }
