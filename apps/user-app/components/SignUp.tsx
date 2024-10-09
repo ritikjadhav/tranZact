d8d2e474f86e90285b9832dba4bff41baf33deea
Ritik Jadhav <ritiksjadhav03@gmail.com>
2024-10-10 00:41:02 +0530

    Refactor: Improve form validation in SignUp component

--- apps/user-app/components/SignUp.tsx
+++ apps/user-app/components/SignUp.tsx
@@ -13,9 +13,18 @@ export const SignUp = () => {
     const [password, setPassword] = useState('')
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
+    const [phoneError, setPhoneError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
     const router = useRouter()
 
-    const handleLogin = async () => {
+    const handleSignup = async () => {
+        setPhoneError(phone.length < 1)
+        setPasswordError(password.length < 1)
+
+        if (phoneError || passwordError) {
+            return
+        }
+
         const res = await signIn('credentials', {
             redirect: false,
             name,
@@ -35,11 +44,37 @@ export const SignUp = () => {
         <div className='sm:w-2/3 lg:w-1/3 mx-6'>
             <AuthHeader label='Sign up' />
             <form>
-                <TextInput type='text' placeholder='Name' onChange={(value) => setName(value)} />
-                <TextInput type='text' placeholder='Mobile Number' onChange={(value) => setPhone(value)} />
-                <TextInput type='text' placeholder='Email' onChange={(value) => setEmail(value)} />
-                <TextInput type='password' placeholder='Password' onChange={(value) => setPassword(value)} />
-                <Button onClick={handleLogin}>Sign up</Button>
+                <TextInput
+                    type='text'
+                    placeholder='Name'
+                    onChange={(value) => setName(value)}
+                />
+                <TextInput
+                    type='text'
+                    placeholder='Mobile Number'
+                    required
+                    emptyInputError={phoneError}
+                    onChange={(value) => {
+                        setPhone(value)
+                        setPhoneError(value.length < 1)
+                    }}
+                />
+                <TextInput
+                    type='text'
+                    placeholder='Email'
+                    onChange={(value) => setEmail(value)}
+                />
+                <TextInput
+                    type='password'
+                    placeholder='Password'
+                    required
+                    emptyInputError={passwordError}
+                    onChange={(value) => {
+                        setPassword(value)
+                        setPasswordError(value.length < 1)
+                    }}
+                />
+                <Button onClick={handleSignup}>Sign up</Button>
             </form>
             <AuthProviders to='/signin' label='Log in now' />
         </div>
